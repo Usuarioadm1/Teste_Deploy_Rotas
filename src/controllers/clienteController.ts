ca3244838dedf0da395e6c6a59f8a8913024e776
Usuarioadm1 <ususuario.adm1@gmail.com>
2024-10-17 15:39:54 -0400

    atualizacao de crud do cliente

--- src/controllers/clienteController.ts
+++ src/controllers/clienteController.ts
@@ -35,21 +35,11 @@ const prisma = new PrismaClient();
 
 export const criarCliente = async function (req: Request, res: Response) {
     try {
-        console.log("teste")
-        const { contatos, dependentes, apolice, corretorId, ...clienteData } = req.body;
-
-        // Primeiro, crie a apólice, se ela não existir
-        const novaApolice = await prisma.apolice.create({
-            data: {
-                nome: apolice.nome,
-                obs: apolice.obs,
-            },
-        });
+        const { contatos, dependentes, apoliceId, corretorId, ...clienteData } = req.body;
 
         const data: Prisma.ClienteCreateInput = {
             ...clienteData,
-            operadora: clienteData.operadora,
-            corretor: { connect: { id: corretorId } }, // Conectar o corretor existente
+            corretor: { connect: { id: corretorId } }, 
             contatos: {
                 create: contatos as Prisma.ContatoCreateInput[],
             },
@@ -57,10 +47,13 @@ export const criarCliente = async function (req: Request, res: Response) {
                 create: dependentes as Prisma.DependenteCreateInput[],
             },
             apoliceCliente: {
-                create: {
-                    apiliceId: novaApolice.id, // Conectar a nova apólice criada
-                },
+                create: [
+                    {
+                        apolice: { connect: { id: apoliceId } }
+                    }
+                ]
             },
+
         };
 
         const novoCliente = await prisma.cliente.create({
@@ -73,40 +66,3 @@ export const criarCliente = async function (req: Request, res: Response) {
         res.status(500).json({ error: 'Houve um erro ao criar o cliente' });
     }
 };
-
-
-// import { PrismaClient, Prisma } from '@prisma/client';
-// import { Request, Response } from 'express';
-
-// const prisma = new PrismaClient();
-
-// export const criarCliente = async function (req: Request, res: Response) {
-//     try {
-//         const { contatos, dependentes, ...clienteData } = req.body;
-
-//         const data: Prisma.ClienteCreateInput = {
-//             ...clienteData,
-//             operadora :{
-
-//             },
-//             corretorId:{
-                
-//             },
-//             contatos: {
-//                 create: contatos as Prisma.ContatoCreateInput[], // Certifique-se de que ContatoCreateInput esteja correto
-//             },
-//             dependentes: {
-//                 create: dependentes as Prisma.DependenteCreateInput[], // Certifique-se de que DependenteCreateInput esteja correto
-//             },
-//         };
-
-//         const novoCliente = await prisma.cliente.create({
-//             data,
-//         });
-
-//         res.status(201).json(novoCliente);
-//     } catch (error) {
-//         console.error("Houve um erro ao criar o cliente:", error);
-//         res.status(500).json({ error: 'Houve um erro ao criar o cliente' });
-//     }
-// };
--- src/routes/clienteRoute.ts
+++ src/routes/clienteRoute.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { criarCliente } from '../controllers/clienteController';
+import { nextTick } from 'process';
 //import { verificarToken } from '../middleware/authMiddleware'
 
 const router = express.Router();
 
-router.get('/criar', criarCliente);
+router.post('/criar',criarCliente);
+
 
 export default router;
-
 
